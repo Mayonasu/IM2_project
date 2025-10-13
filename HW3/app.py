c919ed369ab4dbbeb8796797fea201f4904447c5
Mayonasu <22ur1016@psu.edu.ph>
2025-10-13 20:09:31 +0800

    HW3

---
+++ HW3/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('register.html')
+
+@app.route('/output', methods=['POST'])
+def output():
+    lastname = request.form['lastname']
+    firstname = request.form['firstname']
+    sex = request.form['sex']
+    institution = request.form['institution']
+    email = request.form['email']
+    
+    return render_template(
+        'output.html',
+        lastname=lastname,
+        firstname=firstname,
+        sex=sex,
+        institution=institution,
+        email=email
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
